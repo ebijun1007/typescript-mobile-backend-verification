075d1ffaf3a3cda63afa9904cf43223183f34cc0
ebijun1007@gmail.com <ebijun1007@gmail.com>
2023-04-09 07:33:25 +0900

    nestjsがLambda上で動くようにコードを変更

--- backend/app/src/main.ts
+++ backend/app/src/main.ts
@@ -1,8 +1,38 @@
+import { Server } from 'http';
+import { createServer, proxy } from 'aws-serverless-express';
 import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import * as express from 'express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const binaryMimeTypes: string[] = [];
+let cachedServer: Server;
+
+async function bootstrapServer(): Promise<Server> {
+  if (!cachedServer) {
+    const expressApp = express();
+    const nestApp = await NestFactory.create(
+      AppModule,
+      new ExpressAdapter(expressApp),
+    );
+    nestApp.setGlobalPrefix('v1'); // API のベースパス。詳細後述
+    nestApp.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*')
+      res.header('Access-Control-Allow-Headers', '*')
+      res.header(
+        'Access-Control-Allow-Methods',
+        'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
+      )
+      next()
+    });
+    await nestApp.init();
+    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+  }
+  return cachedServer;
 }
-bootstrap();
+
+// Lambdaに渡されるhandler
+export const handler = async (event: any, context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
