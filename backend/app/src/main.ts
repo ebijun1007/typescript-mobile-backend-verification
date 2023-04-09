f90d222c02ba1de6d01d69c963079d92512811c0
ebijun1007@gmail.com <ebijun1007@gmail.com>
2023-04-09 12:59:12 +0900

    serverless offlineでnestjsのAPIの動作確認まで完了。LocalStackは諦め

--- backend/app/src/main.ts
+++ backend/app/src/main.ts
@@ -1,38 +1,23 @@
-import { Server } from 'http';
-import { createServer, proxy } from 'aws-serverless-express';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
+import serverlessExpress from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
-import * as express from 'express';
 
-const binaryMimeTypes: string[] = [];
-let cachedServer: Server;
+let server: Handler;
 
-async function bootstrapServer(): Promise<Server> {
-  if (!cachedServer) {
-    const expressApp = express();
-    const nestApp = await NestFactory.create(
-      AppModule,
-      new ExpressAdapter(expressApp),
-    );
-    nestApp.setGlobalPrefix('v1'); // API のベースパス。詳細後述
-    nestApp.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*')
-      res.header('Access-Control-Allow-Headers', '*')
-      res.header(
-        'Access-Control-Allow-Methods',
-        'DELETE,GET,HEAD,OPTIONS,PATCH,POST,PUT',
-      )
-      next()
-    });
-    await nestApp.init();
-    cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
-  }
-  return cachedServer;
+async function bootstrap(): Promise<Handler> {
+  const app = await NestFactory.create(AppModule);
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
 
-// Lambdaに渡されるhandler
-export const handler = async (event: any, context) => {
-  cachedServer = await bootstrapServer();
-  return proxy(cachedServer, event, context, 'PROMISE').promise;
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap());
+  return server(event, context, callback);
 };
--- backend/lib/backend-stack.ts
+++ backend/lib/backend-stack.ts
@@ -1,16 +1,50 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from '@aws-cdk/core';
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as apigw from "@aws-cdk/aws-apigateway";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class BackendStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    // Lambda layer
+    const lambdaLayer = new lambda.LayerVersion(this, `NestApplambdaLayer`, {
+      code: lambda.Code.fromAsset("app/node_modules"),
+      compatibleRuntimes: [
+        lambda.Runtime.NODEJS_16_X,
+      ],
+    });
 
-    // The code that defines your stack goes here
+    // Lambda
+    const appLambda = new lambda.Function(this, `NestApplambda`, {
+      runtime: lambda.Runtime.NODEJS_16_X,
+      code: lambda.Code.fromAsset("app/dist"),
+      handler: "main.handler",
+      layers: [lambdaLayer],
+      environment: {
+        NODE_PATH: "$NODE_PATH:/opt",
+        AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
+      },
+      timeout: cdk.Duration.seconds(30),
+    });
+    // API Gateway
+    const restApi = new apigw.RestApi(this, `NestAppApiGateway`, {
+      restApiName: `NestAppApiGw`,
+      deployOptions: {
+        stageName: "v1",
+      },
+      // CORS設定
+      defaultCorsPreflightOptions: {
+        // warn: 要件に合わせ適切なパラメータに絞る
+        allowOrigins: apigw.Cors.ALL_ORIGINS,
+        allowMethods: apigw.Cors.ALL_METHODS,
+        allowHeaders: apigw.Cors.DEFAULT_HEADERS,
+        statusCode: 200,
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BackendQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    restApi.root.addProxy({
+      defaultIntegration: new apigw.LambdaIntegration(appLambda),
+      anyMethod: true
+    });
   }
 }
